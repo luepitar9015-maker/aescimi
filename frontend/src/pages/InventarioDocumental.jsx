441830cd326d1fc209bcfa50237682e20d469703
luepitar9015-maker <luepitar9015@gmail.com>
2026-04-14 17:10:00 -0500

    cambios en frontend

--- frontend/src/pages/InventarioDocumental.jsx
+++ frontend/src/pages/InventarioDocumental.jsx
@@ -195,8 +195,8 @@ function InventarioDocumental() {
                                 <tr className="bg-gray-100 text-[9px]">
                                     <th className="p-1 border text-center">Ini</th>
                                     <th className="p-1 border text-center">Fin</th>
-                                    <th className="p-1 border text-center w-12">Cajas</th>
-                                    <th className="p-1 border text-center w-12">Carp..</th>
+                                    <th className="p-1 border text-center w-12">Caja</th>
+                                    <th className="p-1 border text-center w-12">Carp.</th>
                                     <th className="p-1 border text-center w-12">Tomo</th>
                                     <th className="p-1 border text-center w-12">Otro</th>
                                     <th className="p-1 border text-center w-12">Del</th>
