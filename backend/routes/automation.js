7d68ffffb93007a435295fc38d76eba56392453f
luepitar9015-maker <luepitar9015@gmail.com>
2026-06-09 15:52:36 -0500

    clean: remove diagnostic endpoints and files

--- backend/routes/automation.js
+++ backend/routes/automation.js
@@ -8,9 +8,6 @@ router.post('/unity-execute', requireAuth, requireAdmin, automationController.ex
 router.get('/stream', requireAuth, requireAdmin, automationController.streamAutomation);
 router.post('/diagnostic', requireAuth, requireAdmin, automationController.executeDiagnostic);
 router.post('/unity-robot', requireAuth, requireAdmin, automationController.runUnityRobot);
-router.get('/pm2-logs', automationController.getPM2Logs);
-router.post('/query-db', automationController.queryDb);
-router.post('/exec-command', automationController.execCommand);
 
 module.exports = router;
 
