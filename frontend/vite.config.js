6113625142c5a9c5073f04255bb74538f5f8f553
luepitar9015-maker <luepitar9015@gmail.com>
2026-07-23 21:37:59 -0500

    Restore default Vite hashed assets to solve client-side browser caching issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,15 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: './',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
-  },
+
   server: {
     port: 3000,
     host: true,
